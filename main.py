38c627e423a65e53ac4bd3888fe543ab070c3c70
unknown <d.pucula@gmail.com>
2022-08-17 00:16:06 +0200

    added database.py, and first table

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+from turtle import title
 from fastapi import FastAPI
+from sqlalchemy import Column, Integer, String
 from utils.models import Post
 from utils.database import Base
 
 
 app = FastAPI()
 
+class Post_db(Base):
+
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    
 
 @app.get('/')
 async def root():
